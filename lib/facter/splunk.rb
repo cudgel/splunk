8cae1ebbf9f7f42dae283496b1f041aa49be5512
Christopher Jon Caldwell <caldwell@gwu.edu>
2014-10-27 15:30:48 -0400

    --wip--

--- lib/facter/splunk.rb
+++ lib/facter/splunk.rb
@@ -1,5 +1,10 @@
 Facter.add(:splunk_version) do
-  setcode do
-    version = Facter::Util::Resolution.exec('echo `ls -1 ~splunk/*manifest | sort -n | tail -1` | awk -F- \'{print $2 "-" $3}\'')
+  if Dir.exists?('~splunk')
+    setcode do
+      version = nil
+      version = Facter::Util::Resolution.exec('echo `ls -1 ~splunk/*manifest | sort -n | tail -1` | awk -F- \'{print $2 "-" $3}\'')
+      version = nil unless version =~ /^\d.\d.\d-\d*$/
+      version
+    end
   end
 end
