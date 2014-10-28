3b8e7cdbd4ed273b6fce1960d659b07939c3b696
Christopher Jon Caldwell <caldwell@gwu.edu>
2014-10-28 10:31:09 -0400

    Working splunk_version fact for upgrades

---
+++ lib/facter/splunk_version.rb
@@ -0,0 +1,9 @@
+Facter.add(:splunk_version) do
+  version = nil
+  splunk_home = Facter::Util::Resolution.exec('grep splunk /etc/passwd | awk -F: \'{print $6}\'')
+  version = Facter::Util::Resolution.exec("ls -1 #{splunk_home}/*manifest | sort -n | tail -1 | grep -oE '[[:digit:]].[[:digit:]].[[:digit:]]-[[:digit:]]+'")
+  setcode do
+  # Sanity check to reject anything that is not an iqn
+    splunk_version = version
+  end
+end
--- lib/facter/splunk_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-version=`ls -1 ~splunk/splunk*manifest | sort -n | tail -1 | awk -F- '{print $2"-"$3}'`
-echo "splunk_version: $version"
