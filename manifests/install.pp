c8e185f3ca6d113545d0a6e66fff422bc998508f
Christopher Jon Caldwell <caldwell@gwu.edu>
2015-03-02 08:46:55 -0500

    updates to support mserver

--- manifests/install.pp
+++ manifests/install.pp
@@ -41,7 +41,7 @@ class splunk::install($type=$type)
       }
 
       exec { 'firstStart':
-        command     => "splunk stop; splunk --accept-license --answer-yes --no-prompt start",
+        command     => 'splunk stop; splunk --accept-license --answer-yes --no-prompt start',
         path        => "${::splunk::splunkhome}/bin:/bin:/usr/bin:",
         subscribe   => Exec['unpackSplunk'],
         refreshonly => true,
