603020544b3b296139ca0b608640b8945f60be8b
Christopher Caldwell <caldwell@silicon.local>
2016-08-11 10:52:50 -0400

    specify provider

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,10 @@
 class splunk::service inherits ::splunk {
 
   service { 'splunk':
-        ensure  => 'running',
-        enable  => true,
-        require => Class['::splunk::install']
+        ensure   => 'running',
+        enable   => true,
+        provider => init,
+        require  => Class['::splunk::install']
   }
 
 }
