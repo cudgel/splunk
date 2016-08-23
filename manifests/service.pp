d808255fe650838137859708e3480b0274da9540
Christopher Caldwell <caldwell@silicon.local>
2016-08-23 10:53:54 -0400

    try to handle provider better

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,21 @@
 class splunk::service inherits ::splunk {
 
+  case $::osfamily {
+      'RedHat': {
+          $init = 'redhat'
+        }
+      'Debian': {
+          $init = 'debian'
+      }
+      default: {
+          $init = 'init'
+      }
+  }
+
   service { 'splunk':
         ensure   => 'running',
         enable   => true,
-        provider => init,
+        provider => $init,
         require  => Class['::splunk::install']
   }
 
