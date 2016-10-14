7f6c7a5b90e3310e3f465ff05e593385674e2039
Christopher Caldwell <caldwell@silicon.local>
2016-10-14 09:13:55 -0400

    merges from master

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,22 +1,9 @@
 class splunk::service inherits ::splunk {
 
-  case $::osfamily {
-      'RedHat': {
-          $init = 'redhat'
-        }
-      'Debian': {
-          $init = 'debian'
-      }
-      default: {
-          $init = 'init'
-      }
-  }
-
   service { 'splunk':
-        ensure   => 'running',
-        enable   => true,
-        provider => $init,
-        require  => Class['::splunk::install']
+        ensure  => 'running',
+        enable  => true,
+        require => Class['::splunk::install']
   }
 
 }
