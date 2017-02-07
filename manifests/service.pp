5d4ea6415897a014eabe700078167af888985555
Christopher Caldwell <caldwell@silicon.local>
2017-02-07 10:58:04 -0500

    merges from master

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,24 @@
 class splunk::service inherits ::splunk {
 
+  if $::osfamily == 'Debian' {
+    file { '/lib/systemd/system/splunk.service':
+      ensure => file,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/splunk/splunk.service',
+    }
   service { 'splunk':
         ensure  => 'running',
         enable  => true,
         require => Class['::splunk::install']
   }
-
+  }
+    elsif $::osfamily == 'RedHat' {
+    service { 'splunk':
+        ensure  => 'running',
+        enable  => true,
+        require => Class['::splunk::install']
+    }
+    }
 }
