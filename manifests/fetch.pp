db3ceba51d2438e2997c75c09a87d2d72fb13a71
Christopher Caldwell <caldwell@gwu.edu>
2017-09-18 11:25:12 -0400

    --wip-- [skip ci]

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -8,7 +8,7 @@ define splunk::fetch(
   exec{"retrieve_${splunksource}":
     command => "/usr/bin/wget -O ${splunksource} https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=${version}&product=${sourcepart}&filename=${sourcepart}-${version}-${release}-Linux-x86_64.tgz&wget=true",
     cwd     => $::splunk::install_path,
-    creates => "$::splunk::install_path}/$$splunksource",
+    creates => "$::splunk::install_path}/$splunksource",
     user    => $::splunk::splunk_user,
     group   => $::splunk::splunk_group
   }
