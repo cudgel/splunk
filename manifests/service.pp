9d64ec0517768c98bd6f0106792b54950b3aa1f0
Christopher Caldwell <caldwell@gwu.edu>
2017-12-13 09:53:25 -0500

    test default service for debian

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,24 +2,24 @@
 #
 class splunk::service inherits ::splunk {
 
-  if $::osfamily == 'Debian' {
-    file { '/lib/systemd/system/splunk.service':
-      ensure => file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-      source => 'puppet:///modules/splunk/splunk.service',
-      }
-    service { 'splunk':
-        ensure  => 'running',
-        enable  => true,
-        require => Class['::splunk::install']
-      }
-  } elsif $::osfamily == 'RedHat' {
+  # if $::osfamily == 'Debian' {
+  #   file { '/lib/systemd/system/splunk.service':
+  #     ensure => file,
+  #     owner  => 'root',
+  #     group  => 'root',
+  #     mode   => '0644',
+  #     source => 'puppet:///modules/splunk/splunk.service',
+  #     }
+  #   service { 'splunk':
+  #       ensure  => 'running',
+  #       enable  => true,
+  #       require => Class['::splunk::install']
+  #     }
+  # } elsif $::osfamily == 'RedHat' {
     service { 'splunk':
       ensure  => 'running',
       enable  => true,
       require => Class['::splunk::install']
     }
-  }
+  # }
 }
