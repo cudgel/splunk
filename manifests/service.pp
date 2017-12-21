c321eb6d5b751c736cb030a09f15d56e0ef3aa39
Christopher Caldwell <caldwell@gwu.edu>
2017-12-21 14:08:12 -0500

    try removing requires from service

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,9 +2,8 @@
 #
 class splunk::service inherits ::splunk {
   service { 'splunk':
-    ensure  => 'running',
-    alias   => 'splunk-service',
-    enable  => true,
-    require => File['/etc/init.d/splunk']
+    ensure => 'running',
+    alias  => 'splunk-service',
+    enable => true
   }
 }
