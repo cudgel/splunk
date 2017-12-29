d0283c60f2dc28454352b254686c3f2514f9a267
Christopher Caldwell <caldwell@gwu.edu>
2017-12-29 13:03:42 -0500

    clean up comments from cut/paste, remove unused files

--- lib/facter/splunk_home.rb
+++ lib/facter/splunk_home.rb
@@ -2,7 +2,6 @@ Facter.add(:splunk_guid) do
   guid = nil
   guid = Facter::Util::Resolution.exec("grep guid /opt/splunk*/etc/instance.cfg | awk '{print $3}'")
   setcode do
-  # Sanity check to reject anything that is not an iqn
     splunk_guid = guid
   end
 end
--- lib/facter/splunk_version.rb
+++ lib/facter/splunk_version.rb
@@ -2,7 +2,6 @@ Facter.add(:splunk_version) do
   version = nil
   version = Facter::Util::Resolution.exec("ls -1 /opt/splunk* | grep manifest | sort -n | tail -1 | grep -oE '[[:digit:]].[[:digit:]].[[:digit:]]-[a-z0-9]+'")
   setcode do
-  # Sanity check to reject anything that is not an iqn
     splunk_version = version
   end
 end
