b4748435a29871e47f5c8695795a19d9313f5225
Christopher Caldwell <cudgel@sparcitecture.com>
2018-12-03 18:08:39 -0500

    manage home

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,10 +6,9 @@ class splunk::user {
   $splunk_group      = $::splunk::splunk_group
 
   user { $splunk_user:
-    ensure     => present,
-    name       => $splunk_user,
-    comment    => 'Splunk service account',
-    gid        => $splunk_group,
-    managehome => false
+    ensure  => present,
+    name    => $splunk_user,
+    comment => 'Splunk service account',
+    gid     => $splunk_group
   }
 }
