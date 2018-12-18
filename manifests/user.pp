382acf9f91e5c5f2bd6f488f2495d60915a73cec
Christopher Caldwell <caldwell@gwu.edu>
2018-12-18 09:59:38 -0500

    ensure managehome is true

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,9 +6,10 @@ class splunk::user {
   $splunk_group      = $::splunk::splunk_group
 
   user { $splunk_user:
-    ensure  => present,
-    name    => $splunk_user,
-    comment => 'Splunk service account',
-    gid     => $splunk_group
+    ensure     => present,
+    name       => $splunk_user,
+    comment    => 'Splunk service account',
+    managehome => true,
+    gid        => $splunk_group
   }
 }
