ce850e7d580f70042ab5ec9e7d495aa9c6846033
Christopher Caldwell <caldwell@gwu.edu>
2018-12-20 14:57:35 -0500

    try adding systemd file

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,13 @@
 class splunk::service {
 
+  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] >= 7  {
+      file { '/etc/systemd/system/multi-user.target.wants/splunk.service':
+        content => template("${module_name}/splunk.service.erb"),
+        owner   => 'root',
+        group   => 'root'
+      }
+  }
+
   service { 'splunk':
     ensure   => 'running',
   }
