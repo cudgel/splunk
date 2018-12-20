23c1e978a01c80d1b07721a21c44cf3b74ba794a
Christopher Caldwell <caldwell@gwu.edu>
2018-12-20 08:44:42 -0500

    log that splunk is already at a higher version

--- manifests/init.pp
+++ manifests/init.pp
@@ -159,9 +159,12 @@ Optional[Hash] $tcpout = undef
     # because the legacy fact does not represent splunk version as
     # version-release, we cut the version from the string.
 
-    if versioncmp($version, $cut_version) > 0 or $cut_version == '' or $cwd != $splunkdir {
+    if versioncmp($version, $cut_version) == 1 or $cut_version == '' or $cwd != $splunkdir {
       class { 'splunk::install': } -> class { 'splunk::config': } -> class { 'splunk::service': }
     } else {
+      if versioncmp($version, $cut_version) == -1 {
+        info('Splunk is already at a higher version.')
+      }
       class { 'splunk::config': } -> class { 'splunk::service': }
     }
 
