5bcacd7d0901df6944db612c654803446cb9beaf
Christopher Caldwell <caldwell@gwu.edu>
2018-12-20 15:17:21 -0500

    convert to int

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 class splunk::service {
 
-  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] >= 7  {
+  if $facts['os']['family'] == 'RedHat' and Integer($facts['os']['release']['major']) >= 7  {
       file { '/etc/systemd/system/multi-user.target.wants/splunk.service':
         content => template("${module_name}/splunk.service.erb"),
         owner   => 'root',
