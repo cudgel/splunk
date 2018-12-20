5ca5fb6e17da094cc8b823e6c0071770c2388ab9
Christopher Caldwell <caldwell@gwu.edu>
2018-12-20 12:40:00 -0500

    try defaults for service again

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,6 @@
 class splunk::service {
 
-if $::osfamily == 'Redhat' {
-  $my_provider = 'redhat'
-} else {
-  $my_provider = 'init'
-}
-
   service { 'splunk':
     ensure   => 'running',
-    alias    => 'splunk-service',
-    provider => $my_provider
   }
 }
