4cb2633ec5f911f76bebbdfa8c125de588895c03
Christopher Caldwell <caldwell@gwu.edu>
2019-01-07 13:11:10 -0500

    add message about wrong directory, remove cwd from exec

--- manifests/install.pp
+++ manifests/install.pp
@@ -52,11 +52,11 @@ class splunk::install
 
   # clean up a splunk instance running out of the wrong directory for this role
   if $my_cwd != $dir and $my_cwd != '' and $home != $my_cwd {
+    info("Splunk running out of wrong directory. Should be ${dir}, is ${my_cwd}.")
 
     exec { 'uninstallSplunkService':
       command => 'splunk disable boot-start',
       path    => "${my_cwd}/bin:/bin:/usr/bin:",
-      cwd     => $my_cwd,
       returns => [0, 8]
     }
 
@@ -68,10 +68,10 @@ class splunk::install
       timeout => 600
     }
 
-      file { $my_cwd:
-        ensure => absent,
-        force  => true,
-        backup => false
+    file { $my_cwd:
+      ensure => absent,
+      force  => true,
+      backup => false
     }
     $wsourcepart = basename($my_cwd)
     $wrongsource = "${wsourcepart}-${current_version}-${os}-${arch}.${ext}"
