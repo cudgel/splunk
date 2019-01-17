a4019c2a8e7922e2e5e1c6454ccaed25eecb20b6
Christopher Caldwell <caldwell@gwu.edu>
2019-01-17 15:47:02 -0500

    test safer fact

--- lib/facter/splunk_cwd.rb
+++ lib/facter/splunk_cwd.rb
@@ -2,7 +2,9 @@
 
 Facter.add(:splunk_cwd) do
   setcode do
-    splunk_cwd = Facter::Util::Resolution.exec("readlink -e /proc/$(pgrep -o splunk)/exe | grep -oE '/opt/(splunk|splunkforwarder)' | uniq")
-    splunk_cwd
+    if File.exist? '/opt/splunk' or File.exist? '/opt/splunkforwarder'
+      splunk_cwd = Facter::Util::Resolution.exec("readlink -e /proc/$(pgrep -o splunk)/exe | grep -oE '/opt/(splunk|splunkforwarder)' | uniq")
+      splunk_cwd
+    end
   end
 end
