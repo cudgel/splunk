13adcb6aa34a6b1cce8c8667662d2369242dd628
Christopher Caldwell <caldwell@gwu.edu>
2019-01-18 08:52:55 -0500

    revert facts

--- lib/facter/splunk_shcluster_id.rb
+++ lib/facter/splunk_shcluster_id.rb
@@ -2,9 +2,7 @@
 
 Facter.add(:splunk_shcluster_id) do
   setcode do
-    if File.exist?('/opt/splunk') || File.exist?('/opt/splunkforwarder')
-      splunk_shcluster_id = Facter::Util::Resolution.exec("grep -e \"^id\" /opt/splunk/etc/system/local/server.conf | cut -d'=' -f2 | tr -d '[:space:]'")
-      splunk_shcluster_id
-    end
+    splunk_shcluster_id = Facter::Util::Resolution.exec("grep -e \"^id\" /opt/splunk/etc/system/local/server.conf | cut -d'=' -f2 | tr -d '[:space:]'")
+    splunk_shcluster_id
   end
 end
