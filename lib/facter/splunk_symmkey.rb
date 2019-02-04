48ec13067f5fbbd33d673dbce1a99303071e5113
Christopher Caldwell <cudgel@sparcitecture.com>
2019-02-04 18:24:13 -0500

    capture symmkey

--- lib/facter/splunk_symmkey.rb
+++ lib/facter/splunk_symmkey.rb
@@ -2,7 +2,7 @@
 
 Facter.add(:splunk_symmkey) do
   setcode do
-    splunk_symmkey = Facter::Util::Resolution.exec("grep -e \"^\$\d\$\S+\" /opt/splunk/etc/system/local/symmkey.conf || (grep -e \"^pass4SymmKey\" /opt/splunk/etc/system/local/server.conf | cut -d'=' -f2 | tr -d '[:space:]')")
+    splunk_symmkey = Facter::Util::Resolution.exec(" grep -e \"[:punct:][:digit:][:punct:]\" /opt/splunk/etc/system/local/symmkey.conf || (grep -e \"^pass4SymmKey\" /opt/splunk/etc/system/local/server.conf | cut -d'=' -f2 | tr -d '[:space:]')")
     splunk_symmkey
   end
 end
