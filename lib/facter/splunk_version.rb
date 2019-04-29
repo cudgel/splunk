ac7a33d5a336f8fdab788c356d0ac4cfec12bfd6
Christopher Caldwell <cudgel@sparcitecture.com>
2019-04-29 10:46:21 -0400

    improve pattern match for version

--- lib/facter/splunk_version.rb
+++ lib/facter/splunk_version.rb
@@ -2,7 +2,7 @@
 
 Facter.add(:splunk_version) do
   setcode do
-    splunk_version = Facter::Util::Resolution.exec("ls -1 /opt/splunk* | grep manifest | sort -n | tail -1 | grep -oE '[[:digit:]].[[:digit:]].[[:digit:]]-[a-z0-9]+'")
+    splunk_version = Facter::Util::Resolution.exec("ls -1 /opt/splunk* | grep manifest | sort -n | tail -1 | grep -oE '([0-9.])+-([a-f0-9])+'")
     splunk_version
   end
 end
