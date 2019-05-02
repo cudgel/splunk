af428f6c6254bab2af22cb3aabf3a530503fca81
Christopher Caldwell <cudgel@sparcitecture.com>
2019-05-02 09:01:38 -0400

    use curl instead of wget

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -27,18 +27,18 @@ define splunk::fetch(
   }
   if $source == 'splunk' or source =~ /http.*/  {
     if $source == 'splunk' {
-      $wget_url = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=${version}&product=${product}&filename=${sourcepart}-${version}-${release}-Linux-x86_64.tgz&wget=true"
+      $curl_url = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=${version}&product=${product}&filename=${sourcepart}-${version}-${release}-Linux-x86_64.tgz&wget=true"
     } else {
-      $wget_url = "${source}/${splunk_bundle}"
+      $curl_url = "${source}/${splunk_bundle}"
     }
 
     exec{ "retrieve_${splunk_bundle}":
-      command => "wget -O ${splunk_bundle} \'${wget_url}\'",
+      command => "curl -Lo ${splunk_bundle} \'${curl_url}\'",
       path    => "${::splunk::dir}/bin:/bin:/usr/bin:",
       cwd     => $splunk::install_path,
       timeout => 600,
       creates => "${::splunk::install_path}/${splunk_bundle}",
-      onlyif  => 'wget --server-response https://www.splunk.com -O /dev/null 2>&1'
+      onlyif  => 'curl -I https://www.splunk.com -o /dev/null 2>&1'
     }
 
     file{ "${::splunk::install_path}/${splunk_bundle}":
