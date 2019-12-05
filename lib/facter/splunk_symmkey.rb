8be68eef79a1978ccf8174141e570c3ab8d08775
Christopher Caldwell <cudgel@sparcitecture.com>
2019-12-05 11:42:40 -0500

    avoid multiple keys stacking

--- lib/facter/splunk_symmkey.rb
+++ lib/facter/splunk_symmkey.rb
@@ -5,7 +5,7 @@ Facter.add(:splunk_symmkey) do
     splunk_symmkey = if File.exist?('/opt/splunk/etc/system/local/symmkey.conf')
                        Facter::Util::Resolution.exec('cat /opt/splunk/etc/system/local/symmkey.conf')
                      else
-                       Facter::Util::Resolution.exec("grep -e \"^pass4SymmKey\" /opt/splunk/etc/system/local/server.conf | cut -d'=' -f2 | tr -d '[:space:]'")
+                       Facter::Util::Resolution.exec("grep -e \"^pass4SymmKey\" /opt/splunk/etc/system/local/server.conf | head 1 | cut -d'=' -f2 | tr -d '[:space:]'")
                      end
     splunk_symmkey
   end
