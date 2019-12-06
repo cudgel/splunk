b987cf73faef17f94cf2a0a9f6b80bc4051f601d
Christopher Caldwell <cudgel@sparcitecture.com>
2019-12-06 09:27:26 -0500

    not on known systemd

--- manifests/service.pp
+++ manifests/service.pp
@@ -20,38 +20,40 @@ class splunk::service {
   $user = $splunk::user
 
   if $user == 'splunk' {
-    exec { 'test_for_init':
-      command => 'test -f /etc/init.d/splunk',
-      path    => '/bin:/bin:/usr/bin',
-      unless  => 'test -f /etc/init.d/splunk'
-    }
+    unless $facts['os']['family'] == 'RedHat' and Integer($facts['os']['release']['major']) >= 7 {
+      exec { 'test_for_init':
+        command => 'test -f /etc/init.d/splunk',
+        path    => '/bin:/bin:/usr/bin',
+        unless  => 'test -f /etc/init.d/splunk'
+      }
 
-    file_line { 'splunk-start':
-      path    => '/etc/init.d/splunk',
-      line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" start --no-prompt --answer-yes\'",
-      match   => "^\s\s\"${dir}/bin/splunk\" start",
-      require => Exec['test_for_init']
-    }
+      file_line { 'splunk-start':
+        path    => '/etc/init.d/splunk',
+        line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" start --no-prompt --answer-yes\'",
+        match   => "^\s\s\"${dir}/bin/splunk\" start",
+        require => Exec['test_for_init']
+      }
 
-    file_line { 'splunk-stop':
-      path    => '/etc/init.d/splunk',
-      line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" stop\'",
-      match   => "^\s\s\"${dir}/bin/splunk\" stop",
-      require => Exec['test_for_init']
-    }
+      file_line { 'splunk-stop':
+        path    => '/etc/init.d/splunk',
+        line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" stop\'",
+        match   => "^\s\s\"${dir}/bin/splunk\" stop",
+        require => Exec['test_for_init']
+      }
 
-    file_line { 'splunk-restart':
-      path    => '/etc/init.d/splunk',
-      line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" restart\'",
-      match   => "^\s\s\"${dir}/bin/splunk\" restart",
-      require => Exec['test_for_init']
-    }
+      file_line { 'splunk-restart':
+        path    => '/etc/init.d/splunk',
+        line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" restart\'",
+        match   => "^\s\s\"${dir}/bin/splunk\" restart",
+        require => Exec['test_for_init']
+      }
 
-    file_line { 'splunk-status':
-      path    => '/etc/init.d/splunk',
-      line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" status\'",
-      match   => "^\s\s\"${dir}/bin/splunk\" status",
-      require => Exec['test_for_init']
+      file_line { 'splunk-status':
+        path    => '/etc/init.d/splunk',
+        line    => "  su - ${user} -c \'\"${dir}/bin/splunk\" status\'",
+        match   => "^\s\s\"${dir}/bin/splunk\" status",
+        require => Exec['test_for_init']
+      }
     }
   }
 
