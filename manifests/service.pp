9d2f685d217fa0ee4faadee6a7372f6cbe79cace
Christopher Caldwell <cudgel@sparcitecture.com>
2020-07-02 15:40:36 -0400

    force provider

--- manifests/service.pp
+++ manifests/service.pp
@@ -60,19 +60,24 @@ class splunk::service {
       $start   = "/usr/bin/sudo -u ${user} ${dir}/bin/splunk start"
       $stop    = "/usr/bin/sudo -u ${user} ${dir}/bin/splunk stop"
       $status  = "/usr/bin/sudo -u ${user} ${dir}/bin/splunk status"
+
+      $provider = init
     } else {
       $restart = '/usr/bin/sudo /usr/bin/systemctl restart Splunkd.service'
       $start   = '/usr/bin/sudo /usr/bin/systemctl start Splunkd.service'
       $stop    = '/usr/bin/sudo /usr/bin/systemctl stop Splunkd.service'
       $status  = '/usr/bin/sudo /usr/bin/systemctl status Splunkd.service'
+
+      $provder = systemd
     }
   }
 
   service { 'splunk':
-    ensure  => 'running',
-    restart => $restart,
-    start   => $start,
-    stop    => $stop,
-    status  => $status
+    ensure   => 'running',
+    restart  => $restart,
+    start    => $start,
+    stop     => $stop,
+    status   => $status,
+    provider => $provider
   }
 }
