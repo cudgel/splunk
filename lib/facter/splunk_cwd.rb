f3fe433de5cede575133598cebc98da109112ef1
Christopher Caldwell <cudgel@sparcitecture.com>
2020-07-29 14:30:36 -0400

    change quoting

--- lib/facter/splunk_cwd.rb
+++ lib/facter/splunk_cwd.rb
@@ -2,7 +2,7 @@
 
 Facter.add(:splunk_cwd) do
   setcode do
-    splunk_cwd = Facter::Util::Resolution.exec('find /opt -maxdepth 1 -type d -name \"splunk*\"')
+    splunk_cwd = Facter::Util::Resolution.exec("find /opt -maxdepth 1 -type d -name \"splunk*\"")
     splunk_cwd
   end
 end
