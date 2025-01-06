e47c616a8a46ad0c051a7bcaae39db453f539bda
Christopher Caldwell <cudgel@users.noreply.github.com>
2025-01-06 14:02:38 -0500

    add testacl before applying acl.

--- manifests/acl.pp
+++ manifests/acl.pp
@@ -21,7 +21,7 @@ define splunk::acl(
     $object = $target
   }
   if $recurse != true and $recurse != false {
-      fail('variable "recurse" must be either true or false')
+    fail('variable "recurse" must be either true or false')
   }
 
   if $facts['kernel'] == 'Linux' {
@@ -41,65 +41,21 @@ define splunk::acl(
     $acl = "group:${group}:${perm}"
     $gacl = "group:${group}:r-x"
 
-    # returns 0 if the mount containing the object suports ACLs
-    $testacl = "which getfacl && df -P ${object} | tail -1 | awk '{print \$1}' \
-| fgrep -f - /proc/mounts | grep -q seclabel"
+    # returns 0 if the mount containing the object supports ACLs
+    $testacl = "getfacl -e ${object} > /dev/null 2>&1"
 
-    # Non-recursive ACLs can be applied to anything.
-    #
-    if $recurse == true {
-      $setfacl   = "setfacl -R -m ${acl} ${object} && setfacl -d -R -m ${acl} ${object}"
-    } else {
-      $setfacl   = "setfacl -m ${acl} ${object}"
-    }
-
-    # apply the acl to the object, unless it is an nfs mount
-    # throw an error if acl is not supported on filesystem
-    #
-    exec { "setfacl_${title}":
-      path    => '/bin:/usr/bin',
-      command => $setfacl,
+    exec { "set_acl_${object}":
+      command => "setfacl -m ${acl} ${object}",
       onlyif  => $testacl,
-      unless  => "which getfacl && getfacl ${object} 2>/dev/null | egrep -q '${acl}'",
-      timeout => '0'
+      path    => '/bin:/usr/bin:/sbin:/usr/sbin',
     }
 
-    # set a sane default mask to the object so that group acls work in the
-    # absence of liberal traditional permissions
-    exec { "set_effective_rights_mask_${title}":
-      path    => '/bin:/usr/bin',
-      command => "setfacl -R -m mask:${perm},default:mask:${perm} ${object}",
-      onlyif  => "${testacl} && ${testdir}",
-      unless  => "which getfacl && getfacl ${object} 2>/dev/null | egrep -q '^mask::r-x' ",
-      timeout => '0'
-    }
-
-    if $parents == true {
-      $directories = split($object, '/')
-
-      $directories.each |$index, $directory| {
-        $calculated_dir = inline_template("<%= @directories[0, @index + 1].join('/') %>")
-        $full_path = "/${calculated_dir}"
-        if (! defined(File[$full_path]) and $full_path != '/') {
-          exec { "setfacl_${directory}":
-            path    => '/bin:/usr/bin',
-            command => "setfacl -m ${gacl} ${full_path}",
-            onlyif  => "${testacl} && ${testdir}",
-            unless  => "which getfacl && getfacl ${full_path} 2>/dev/null | egrep -q '${gacl}'",
-            timeout => '0'
-          }
-
-          exec { "set_effective_rights_mask_${directory}":
-            path    => '/bin:/usr/bin',
-            command => "setfacl -m mask:r-x,default:mask:r-x ${full_path}",
-            onlyif  => "${testacl} && ${testdir}",
-            unless  => "which getfacl && getfacl ${full_path} 2>/dev/null | egrep -q '^mask::r-x' ",
-            timeout => '0'
-          }
-        }
+    if $recurse == true {
+      exec { "set_acl_recursive_${object}":
+        command => "setfacl -R -m ${acl} ${object}",
+        onlyif  => $testacl,
+        path    => '/bin:/usr/bin:/sbin:/usr/sbin',
       }
     }
-
   }
-
 }
